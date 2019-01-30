3301f2ae49fda7e4b9110f36d13317aac0c50a3a
qwj2007 <42906316+qwj2007@users.noreply.github.com>
2019-01-30 11:46:17 +0800

    rabbitmq在netcore的简单应用
    rabbitmq在netcore的简单应用

---
+++ DotNetCore.RabbitMQ/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+
+namespace Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			//创建连接工厂
+	        ConnectionFactory factory = new ConnectionFactory
+	        {
+		        UserName = "admin",//用户名
+		        Password = "admin",//密码
+		        HostName = "127.0.0.1"//rabbitmq ip
+	        };
+            //网络异常自动连接恢复
+            factory.AutomaticRecoveryEnabled = true;
+            //创建连接
+            var connection = factory.CreateConnection();
+			//创建通道
+	        var channel = connection.CreateModel();
+			//声明一个队列
+	        channel.QueueDeclare("hello", false, false, false, null);
+
+			Console.WriteLine("\nRabbitMQ连接成功，请输入消息，输入exit退出！");
+
+	        string input;
+            //for (int i = 0; i < 100000; i++)
+            //{
+            //    var sendBytes = Encoding.UTF8.GetBytes(i.ToString());
+            //    //发布消息
+            //    channel.BasicPublish("", "hello", null, sendBytes);
+            //}
+
+            do
+            {
+                input = Console.ReadLine();
+
+                var sendBytes = Encoding.UTF8.GetBytes(input);
+                //发布消息
+                channel.BasicPublish("", "hello", null, sendBytes);
+
+            } while (input.Trim().ToLower() != "exit");
+            channel.Close();
+	        connection.Close();
+
+		}
+    }
+}
---
+++ ProducerWithTopicExchange/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace ProducerWithTopicExchange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			string exchangeName = "TestTopicChange";
+	        string queueName = "hello";
+	        string routeKey = "TestRouteKey.*";
+
+	        //创建连接工厂
+	        ConnectionFactory factory = new ConnectionFactory
+	        {
+		        UserName = "admin",//用户名
+		        Password = "admin",//密码
+		        HostName = "192.168.84.31"//rabbitmq ip
+	        };
+
+	        //创建连接
+	        var connection = factory.CreateConnection();
+	        //创建通道
+	        var channel = connection.CreateModel();
+
+	        //定义一个Direct类型交换机
+	        channel.ExchangeDeclare(exchangeName, ExchangeType.Topic, false, false, null);
+
+	        //定义队列1
+	        channel.QueueDeclare(queueName, false, false, false, null);
+
+	        //将队列绑定到交换机
+	        channel.QueueBind(queueName, exchangeName, routeKey, null);
+
+
+
+	        Console.WriteLine($"\nRabbitMQ连接成功，\n\n请输入消息，输入exit退出！");
+
+	        string input;
+	        do
+	        {
+		        input = Console.ReadLine();
+
+		        var sendBytes = Encoding.UTF8.GetBytes(input);
+	            //持久化
+	            IBasicProperties properties = channel.CreateBasicProperties();
+	            properties.DeliveryMode = 2;//
+                //发布消息
+                channel.BasicPublish(exchangeName, "TestRouteKey.one", null, sendBytes);
+
+	        } while (input.Trim().ToLower() != "exit");
+	        channel.Close();
+	        connection.Close();
+		}
+    }
+}
